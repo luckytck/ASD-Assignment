096446fd295dd2890e3a1b94155f577a29cff6f2
Tan Cheong Kiat <luckytck97@gmail.com>
2017-11-19 18:17:05 +0800

    [Finishes #152572685]"Update Delivery Man Information" Completed Link to database

--- src/Program.java
+++ src/Program.java
@@ -2,21 +2,22 @@
 import DeliveryMan.DeliveryMan;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Program {
-
-    private static List<DeliveryMan> deliveryManList = new ArrayList<>();
-
-    private static void initializeList() {
+    
+    private static <T> List<T> initializeList(String fileName) { //Retrieve List from .dat file
+        List<T> list = new ArrayList<>();
         try {
-            ObjectInputStream oiStream = new ObjectInputStream(new FileInputStream("deliveryMan.dat"));
-            deliveryManList = (ArrayList) (oiStream.readObject());
+            ObjectInputStream oiStream = new ObjectInputStream(new FileInputStream(fileName));
+            list = (ArrayList) (oiStream.readObject());
             oiStream.close();
         } catch (FileNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "File not found", "ERROR", JOptionPane.ERROR_MESSAGE);
@@ -24,6 +25,19 @@ public class Program {
             JOptionPane.showMessageDialog(null, "Cannot read from file", "ERROR", JOptionPane.ERROR_MESSAGE);
         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "Class not found", "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+        return list;
+    }
+
+    private static <T> void updateList(List<T> list, String fileName) { //Store List to .dat file
+        try {
+            ObjectOutputStream ooStream = new ObjectOutputStream(new FileOutputStream(fileName));
+            ooStream.writeObject(list);
+            ooStream.close();
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "File not found", "ERROR", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Cannot save to file", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -39,96 +53,131 @@ public class Program {
             System.out.println("1. HR Executive: Update Delivery Man Information");
             System.out.println("2. Delivery Man: Update Delivery Man Working Status");
             System.out.println("3. Affiliate   : Add Menu");
-            System.out.println("4. Customer    : ");
+            System.out.println("4. Customer    : Place Order");
             System.out.print("Please choose your option (-1 to exit): ");
-            option[0] = scanner.nextInt();
-            scanner.nextLine();
-            if (option[0] != 1 && option[0] != 2 && option[0] != 3 && option[0] != 4 && option[0] != -1) {
-                System.out.println("Invalid Option!!! Please try again~");
-                loop[0] = true;
-            } else if (option[0] == -1) {
-                System.out.println("Thank you for using this system~");
-                System.exit(0);
-            } else if (option[0] == 1) {
-                initializeList();
-                int count;
-                do {
-                    loop[1] = false;
-                    count = 0;
-                    System.out.println("D e l i v e r y   M a n   L i s t");
-                    System.out.println("=================================");
-                    String result = String.format("%-3s %-4s  %-20s   %-6s    %-12s   %-20s   %-14s   %-12s   %-10s\n", "NO.", "ID", "NAME", "GENDER", "CONTACT NO.", "EMAIL", "IC NO.", "BASIC_SALARY", "STATUS");
-                    for (int i = 0; i < deliveryManList.size(); i++) {
-                        if (deliveryManList.get(i) != null) {
-                            result += String.format("%-3s %s", i + 1 + ".", deliveryManList.get(i));
-                            count++;
+            try {
+                option[0] = scanner.nextInt();
+                scanner.nextLine();
+                if (option[0] != 1 && option[0] != 2 && option[0] != 3 && option[0] != 4 && option[0] != -1) {
+                    System.out.println("Invalid Option!!! Please try again~");
+                    loop[0] = true;
+                } else if (option[0] == -1) {
+                    System.out.println("Thank you for using this system~");
+                    System.exit(0);
+                } else if (option[0] == 1) {
+                    List<DeliveryMan> deliveryManList = initializeList("deliveryMan.dat");
+                    int count;
+                    do {
+                        loop[1] = false;
+                        count = 0;
+                        System.out.println("D e l i v e r y   M a n   L i s t");
+                        System.out.println("=================================");
+                        String result = String.format("%-3s %-4s  %-20s   %-6s    %-12s   %-20s   %-14s   %-12s   %-10s\n", "NO.", "ID", "NAME", "GENDER", "CONTACT NO.", "EMAIL", "IC NO.", "BASIC_SALARY", "STATUS");
+                        for (int i = 0; i < deliveryManList.size(); i++) {
+                            if (deliveryManList.get(i) != null) {
+                                result += String.format("%-3s %s", i + 1 + ".", deliveryManList.get(i));
+                                count++;
+                            }
                         }
-                    }
-                    System.out.println(result);
-                    System.out.print("Please choose the delivery man you want to update: ");
-                    option[1] = scanner.nextInt();
-                    scanner.nextLine();
-                    if (option[1] <= 0 || option[1] > count) {
-                        System.out.println("Invalid Option!!! Please try again~");
-                        loop[1] = true;
-                    } else {
-                        String name, contactNumber, email, homeAddress, identityCardNo;
-                        char gender;
-                        double basicSalary;
-                        do {
-                            loop[2] = false;
-                            System.out.println("Please enter the updated details: ");
-                            System.out.print("Name: ");
-                            name = scanner.nextLine().trim();
-                            System.out.print("Gender (M/F): ");
-                            while (!scanner.hasNext("[mfMF]{1}")) {
-                                System.out.println("Invalid Gender!!! Please try again~");
-                                System.out.print("Gender (M/F): ");
-                                scanner.next();
-                            }
-                            gender = scanner.next().toUpperCase().charAt(0);
-                            scanner.nextLine();
-                            System.out.print("Contact No. (01#-########): ");
-                            while (!scanner.hasNext("01[0-9]{1}-[0-9]{7,8}")) {
-                                System.out.println("Invalid Contact No.!!! Please try again~");
-                                System.out.print("Contact No. (01#-########): ");
+                        System.out.println(result);
+                        if (count == 0) {
+                            System.out.println("No delivery man found in database.");
+                        } else {
+                            System.out.print("Please choose the delivery man you want to update (-1 to exit): ");
+                            try {
+                                option[1] = scanner.nextInt();
                                 scanner.nextLine();
-                            }
-                            contactNumber = scanner.nextLine().trim();
-                            System.out.print("Email Address: ");
-                            while (!scanner.hasNext("([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})")) {
-                                System.out.println("Invalid Email Address!!! Please try again~");
-                                System.out.print("Email Address: ");
+                                if ((option[1] <= 0 || option[1] > count) && option[1] != -1) {
+                                    System.out.println("Invalid Option!!! Please choose an option from the list~");
+                                    loop[1] = true;
+                                } else if (option[1] == -1){
+                                    System.out.println("Thank you for using this system~");
+                                    System.exit(0);
+                                } else {
+                                    String name, contactNumber, email, homeAddress, identityCardNo;
+                                    char gender;
+                                    double basicSalary = 0;
+                                    System.out.println("Please enter the new details: ");
+                                    System.out.print("Name: ");
+                                    name = scanner.nextLine().trim();
+                                    System.out.print("Gender (M/F): ");
+                                    while (!scanner.hasNext("[mfMF]{1}")) {
+                                        System.out.println("Invalid Gender!!! Please try again~");
+                                        System.out.print("Gender (M/F): ");
+                                        scanner.next();
+                                    }
+                                    gender = scanner.next().toUpperCase().charAt(0);
+                                    scanner.nextLine();
+                                    System.out.print("Contact No. (01#-########): ");
+                                    while (!scanner.hasNext("01[0-9]{1}-[0-9]{7,8}")) {
+                                        System.out.println("Invalid Contact No.!!! Please try again~");
+                                        System.out.print("Contact No. (01#-########): ");
+                                        scanner.nextLine();
+                                    }
+                                    contactNumber = scanner.nextLine().trim();
+                                    System.out.print("Email Address: ");
+                                    while (!scanner.hasNext("([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})")) {
+                                        System.out.println("Invalid Email Address!!! Please try again~");
+                                        System.out.print("Email Address: ");
+                                        scanner.nextLine();
+                                    }
+                                    email = scanner.nextLine().trim();
+                                    System.out.print("Home Address: ");
+                                    homeAddress = scanner.nextLine().trim();
+                                    System.out.print("Identity Card No. (######-##-####): ");
+                                    while (!scanner.hasNext("[0-9]{6}-[0-9]{2}-[0-9]{4}")) {
+                                        System.out.println("Invalid IC No.!!! Please try again~");
+                                        System.out.print("Identity Card No. (######-##-####): ");
+                                        scanner.nextLine();
+                                    }
+                                    identityCardNo = scanner.nextLine().trim();
+                                    while (basicSalary <= 0) {
+                                        System.out.print("Basic Salary (RM): ");
+                                        while (!scanner.hasNextDouble()) {
+                                            System.out.println("Invalid Value!!! Please enter numeric value only~");
+                                            System.out.print("Basic Salary (RM): ");
+                                            scanner.next();
+                                        }
+                                        basicSalary = scanner.nextDouble();
+                                        if (basicSalary <= 0) {
+                                            System.out.println("Basic Salary must greater than 0!!! Please try again~");
+                                        }
+                                    }
+                                    System.out.print("Confirm to update this delivery man? (Y/N): ");
+                                    char confirmation = scanner.next().toUpperCase().charAt(0);
+                                    if (confirmation == 'Y') {
+                                        DeliveryMan deliveryMan = new DeliveryMan(name, String.valueOf(gender), contactNumber, email, homeAddress, identityCardNo, basicSalary);
+                                        deliveryManList.set(count - 1, deliveryMan);
+                                        updateList(deliveryManList, "deliveryMan.dat");
+                                        System.out.println("Updated Successfully!!!");
+                                    } else {
+                                        System.out.println("Update Cancelled.");
+                                    }
+                                    System.out.print("Do you want to update another delivery man information? (Y/N): ");
+                                    char continueUpdate = scanner.next().toUpperCase().charAt(0);
+                                    if (continueUpdate == 'Y') {
+                                        loop[1] = true;
+                                    }
+                                }
+                            } catch (Exception e) {
+                                System.out.println("Invalid Option!!! Please enter numeric value only~");
                                 scanner.nextLine();
+                                loop[1] = true;
                             }
-                            email = scanner.nextLine().trim();
-                            System.out.print("Home Address: ");
-                            homeAddress = scanner.nextLine().trim();
-                            System.out.print("Identity Card No. (######-##-####): ");
-                            while (!scanner.hasNext("[0-9]{6}-[0-9]{2}-[0-9]{4}")) {
-                                System.out.println("Invalid IC No.!!! Please try again~");
-                                System.out.print("Identity Card No. (######-##-####): ");
-                                scanner.nextLine();
-                            }
-                            identityCardNo = scanner.nextLine().trim();
-                            System.out.print("Basic Salary (RM): ");
-                            while (!scanner.hasNextDouble()) {
-                                System.out.println("Invalid Value!!! Please enter numeric value only~");
-                                System.out.print("Basic Salary (RM): ");
-                                scanner.next();
-                            }
-                            basicSalary = scanner.nextDouble();
-                        } while (loop[2]);
-                    }
-                } while (loop[1]);
-            } else if (option[0] == 2) { //Wong Li Yi
+                        }
+                    } while (loop[1]);
+                } else if (option[0] == 2) { //Wong Li Yi
 
-            } else if (option[0] == 3) { //Tan Qi Han
+                } else if (option[0] == 3) { //Tan Qi Han
 
-            } else if (option[0] == 4) { //Wo Shiou Chein
+                } else if (option[0] == 4) { //Wo Shiou Chein
 
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid Option!!! Please enter numeric value only~");
+                scanner.nextLine();
+                loop[0] = true;
             }
-
         } while (loop[0]);
 
     }
