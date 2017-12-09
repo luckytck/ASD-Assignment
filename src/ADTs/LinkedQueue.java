306a28455674a5e75b0ca9fdcb8559e96a8b0a75
Tan Cheong Kiat <luckytck97@gmail.com>
2017-12-10 03:57:42 +0800

    Create LinkedQueue & QueueInterface

---
+++ src/ADTs/LinkedQueue.java
@@ -0,0 +1,116 @@
+package ADTs;
+
+import java.io.Serializable;
+
+/**
+ * LinkedQueue.java A class that implements the ADT queue by using a circular
+ * doubly chain of nodes that has head references only. NOTE: This version does not
+ * include the setNextNode, getNextNode, setData and getData methods in the Node
+ * class
+ */
+public class LinkedQueue<T> implements QueueInterface<T>, Serializable {
+
+    private Node firstNode;
+    private int numberOfEntries;
+
+    public LinkedQueue() {
+        clear();
+    }
+    
+    private class Node implements Serializable {
+
+        private T data;
+        private Node next;
+        private Node previous;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node next, Node previous) {
+            this.data = data;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+
+    @Override
+    public void enqueue(T newEntry) {
+        if (isEmpty()) {
+            Node newNode = new Node(newEntry);
+            firstNode = newNode;
+            firstNode.next = firstNode;
+            firstNode.previous = firstNode;
+        } else {
+            Node lastNode = firstNode.previous;
+            Node newNode = new Node(newEntry, firstNode, lastNode);
+            lastNode.next = newNode;
+            firstNode.previous = newNode;
+        }
+        numberOfEntries++;
+    }
+
+    @Override
+    public T dequeue() {
+        T data = null;
+        if (!isEmpty()) {
+            data = firstNode.data;
+            if (firstNode == firstNode.next) {
+                firstNode = null;
+            } else {
+                Node lastNode = firstNode.previous;
+                firstNode = firstNode.next;
+                firstNode.previous = lastNode;
+                lastNode.next = firstNode;
+            }
+            numberOfEntries--;
+        }
+        return data;
+    }
+
+    @Override
+    public T getFront() {
+        T data = null;
+        if (!isEmpty()) {
+            data = firstNode.data;
+        }
+        return data;
+    }
+    
+    @Override
+    public int getNumberOfEntries() {
+        return numberOfEntries;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (firstNode == null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        firstNode = null;
+        numberOfEntries = 0;
+    }
+    
+    public static void main(String[] args) {
+        QueueInterface<Integer> queue = new LinkedQueue<>();
+        queue.enqueue(1);
+        System.out.println(queue.dequeue());
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.getFront());
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
