a7c58505e606f6e744076ec4eaa82e7b8325a89d
ShiouChein <ShiouChein@ShiouChein>
2017-12-10 19:56:20 +0800

    modify retrieve cust.

--- src/restaurant/RetrieveCustomer.java
+++ src/restaurant/RetrieveCustomer.java
@@ -9,13 +9,15 @@ import java.util.Scanner;
 public class RetrieveCustomer {
 
     public static final String fileName = "customer.dat";
-    private static String name, address, state, city;
-    private static int postcode;
-    private static char gender;
 
     public static void main(String[] args) {
 
-        String phone;
+        String phone, findPhoneNo, name = "", address = "", state = "", city = "";
+        int postcode = 0;
+        char gender = 0;
+        boolean exist = false;
+
+        ListInterface<Customer> customer = File.retrieveList(fileName);
 
         do {
             Scanner phoneNo = new Scanner(System.in);
@@ -23,42 +25,34 @@ public class RetrieveCustomer {
             phone = phoneNo.next();
 
             if (Validation.ValidateContactNumber(phone) == true) {
-                if (existPhoneNo(phone) == true) {
-                    System.out.println("\nCustomer details");
-                    System.out.println("================");
-                    System.out.println("Customer name: " + name);
-                    System.out.println("Gender: " + gender);
-                    System.out.println("Delivery address: " + address + ", " + postcode + " " + city + ", " + state);
-                } else {
-                    System.out.println("Sorry, no such customer. Thank you");
+                for (int i = 1; i <= customer.getNumberOfEntries(); i++) {
+                    findPhoneNo = customer.getEntry(i).getContactNo();
+
+                    if (findPhoneNo.equals(phone)) {
+                        exist = true;
+                        name = customer.getEntry(i).getName();
+                        address = customer.getEntry(i).getAddress().getAddress();
+                        state = customer.getEntry(i).getAddress().getState();
+                        city = customer.getEntry(i).getAddress().getCity();
+                        postcode = customer.getEntry(i).getAddress().getPostcode();
+                        gender = customer.getEntry(i).getGender();
+                        break;
+                    }
                 }
+                break;
             } else {
-                System.out.println("This number is not valid. Please try again.");
+                System.out.println("This phone number is not valid. Please re-enter.");
             }
         } while (Validation.ValidateContactNumber(phone) == false);
-    }
 
-    private static boolean existPhoneNo(String phoneNo) {
-        boolean exist = false;
-
-        ListInterface<Customer> customer = File.retrieveList(fileName);
-        String phoneNumber;
-
-        for (int i = 1; i <= customer.getNumberOfEntries(); i++) {
-            phoneNumber = customer.getEntry(i).getContactNo();
-
-            if (phoneNumber.equals(phoneNo)) {
-                exist = true;
-                name = customer.getEntry(i).getName();
-                address = customer.getEntry(i).getAddress().getAddress();
-                state = customer.getEntry(i).getAddress().getState();
-                city = customer.getEntry(i).getAddress().getCity();
-                postcode = customer.getEntry(i).getAddress().getPostcode();
-                gender = customer.getEntry(i).getGender();
-                break;
-            }
+        if (exist == true) {
+            System.out.println("\nCustomer details");
+            System.out.println("================");
+            System.out.println("Customer name: " + name);
+            System.out.println("Gender: " + gender);
+            System.out.println("Delivery address: " + address + ", " + postcode + " " + city + ", " + state);
+        } else {
+            System.out.println("Sorry,no such customer in the record. Thank you");
         }
-        return exist;
     }
-
 }
