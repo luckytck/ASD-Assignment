85b145a032a2da49c77a09e6cccc1d89256fcefa
ShiouChein <ShiouChein@ShiouChein>
2017-12-21 16:07:30 +0800

    deliveryman retrieve customer

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import ADTs.ListInterface;
 import ADTs.QueueInterface;
 import Classes.Address;
 import Classes.Affiliate;
+import Classes.Customer;
 import Classes.DeliveryMan;
 import Classes.MenuItem;
 import Classes.File;
@@ -469,7 +470,49 @@ public class Main {
     }
 
     private static void retrieveCustomerDetails() {
+        String phone, findPhoneNo, name = "", address = "", state = "", city = "";
+        int postcode = 0;
+        char gender = 0;
+        boolean exist = false;
 
+        ListInterface<Customer> customer = File.retrieveList(CUSTOMERFILE);
+
+        do {
+            Scanner phoneNo = new Scanner(System.in);
+            System.out.print("Please enter customer phone no.(01x-xxxxxxxx):");
+            phone = phoneNo.next();
+
+            if (Validation.ValidateContactNumber(phone) == true) {
+                for (int i = 1; i <= customer.getNumberOfEntries(); i++) {
+                    findPhoneNo = customer.getEntry(i).getContactNo();
+
+                    if (findPhoneNo.equals(phone)) {
+                        exist = true;
+                        name = customer.getEntry(i).getName();
+                        address = customer.getEntry(i).getAddress().getAddress();
+                        state = customer.getEntry(i).getAddress().getState();
+                        city = customer.getEntry(i).getAddress().getCity();
+                        postcode = customer.getEntry(i).getAddress().getPostcode();
+                        gender = customer.getEntry(i).getGender();
+                        break;
+                    }
+                }
+                break;
+            } else {
+                System.out.println("This phone number is not valid. Please re-enter.");
+            }
+        } while (Validation.ValidateContactNumber(phone) == false);
+
+        if (exist == true) {
+            System.out.println("\nCustomer details");
+            System.out.println("================");
+            System.out.println("Customer name: " + name);
+            System.out.println("Gender: " + gender);
+            System.out.println("Contact No.:" + phone);
+            System.out.println("Delivery address: " + address + ", " + postcode + " " + city + ", " + state);
+        } else {
+            System.out.println("Sorry,no such customer in the record. Thank you");
+        }
     }
 
     private static void updateWorkingStatus(String username) {
